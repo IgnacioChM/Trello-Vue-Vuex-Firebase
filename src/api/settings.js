dfd48ca5cc33ff217ad4ab1eddc29737895fccb9
Nacho <nachochambo@gmail.com>
2021-08-03 20:27:22 +0200

    F3 - Api + Firebase
    Instalación Vuex
    Creación Store
    Configuración Firebase
    API Client
    Mutation Types
    Initial State
    Mutations

---
+++ src/api/settings.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDuzDMjq3c2F7-FkD-3cjyRCZzGKFim2Bw",
+    authDomain: "vue-trello-2c4af.firebaseapp.com",
+    projectId: "vue-trello-2c4af",
+    storageBucket: "vue-trello-2c4af.appspot.com",
+    messagingSenderId: "976000479294",
+    appId: "1:976000479294:web:206de3285561966582a16f",
+    measurementId: "G-0S1BBV5C35"
+}
---
+++ src/store/state.js
@@ -0,0 +1,30 @@
+export default {
+    user: null,
+    fetchingData: true,
+    error: null,
+
+    boards: {/* 
+        id: {
+            id,
+            owner,
+            name
+        }
+    */},
+    lists: {/* 
+        id: {
+            id,
+            board,
+            name
+        }
+    */},
+    tasks: {
+        /* 
+        id: {
+            id,
+            list,
+            title,
+            completed
+        }
+    */
+    }
+}
